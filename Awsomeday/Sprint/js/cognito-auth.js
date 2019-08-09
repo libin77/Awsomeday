7c2e34b32f186cdc4bc0e78dff6a457a412efb9e
Libin77 <slibinlougine@gmail.com>
2019-08-09 08:19:04 +0530

    cognito

--- Awsomeday/Sprint/js/cognito-auth.js
+++ Awsomeday/Sprint/js/cognito-auth.js
@@ -117,7 +117,7 @@ var WildRydes = window.WildRydes || {};
         signin(email, password,
             function signinSuccess() {
                 console.log('Successfully Logged In');
-                window.location.href = 'ride.html';
+                window.location.href = 'chart.html';
             },
             function signinError(err) {
                 alert(err);
